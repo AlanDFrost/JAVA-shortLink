8b567123d5b7e187abfb8c435e8cd5064f22f84d
zyc <zyc15093213381@163.com>
2025-11-12 16:54:01 +0800

    分支测试提交

---
+++ shortlink-admin/src/test/java/org/myprject/shortlink/ShortlinkAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myprject.shortlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortlinkAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
