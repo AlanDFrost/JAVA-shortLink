769e9983b2295a9f499e8494fe647e6e2a748749
zyc <zyc15093213381@163.com>
2025-12-09 17:07:35 +0800

    加入过滤器白名单

--- admin/src/main/java/org/myproject/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/org/myproject/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,12 +1,14 @@
 package org.myproject.shortlink.admin.common.biz.user;
 
 import com.alibaba.fastjson2.JSON;
+import com.google.common.collect.Lists;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * 用户信息传输过滤器
@@ -16,26 +18,31 @@ import java.io.IOException;
 public class UserTransmitFilter implements Filter {
     private final StringRedisTemplate stringRedisTemplate;
 
+    private static final List<String> IGNORE_URI = Lists.newArrayList(
+            "/api/short-link/v1/user/login",
+            "/api/short-link/v1/user/has-username"
+    );
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        String username = httpServletRequest.getHeader("username");
-        String token = httpServletRequest.getHeader("token");
+        String requestURI = httpServletRequest.getRequestURI();
+        if (!IGNORE_URI.contains(requestURI)) {
+            String username = httpServletRequest.getHeader("username");
+            String token = httpServletRequest.getHeader("token");
 
-        if (username == null || token == null) {
+            Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+            if (userInfoJsonStr != null) {
+                UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
+                UserContext.setUser(userInfoDTO);
+            }
+            try {
+                filterChain.doFilter(servletRequest, servletResponse);
+            } finally {
+                UserContext.removeUser();
+            }
+        } else {
             filterChain.doFilter(servletRequest, servletResponse);
-            return;
-        }
-
-        Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
-        if (userInfoJsonStr != null) {
-            UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
-            UserContext.setUser(userInfoDTO);
-        }
-        try {
-            filterChain.doFilter(servletRequest, servletResponse);
-        } finally {
-            UserContext.removeUser();
         }
     }
 }
