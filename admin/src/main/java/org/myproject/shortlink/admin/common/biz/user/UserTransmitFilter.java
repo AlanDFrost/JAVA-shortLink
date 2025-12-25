17a5d6bcb8fc27ccf0c4c0bd3229e0627be5366c
zyc <zyc15093213381@163.com>
2025-12-25 14:54:32 +0800

    拦截器封装用户上下文（临时，后面要在网关力判断），加了打包的依赖

--- admin/src/main/java/org/myproject/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/org/myproject/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,14 +1,19 @@
 package org.myproject.shortlink.admin.common.biz.user;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
 import com.google.common.collect.Lists;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.myproject.shortlink.admin.common.convention.exception.ClientException;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
+
+import static org.myproject.shortlink.admin.common.enums.UserErrorCodeEnum.USER_TOKEN_FAIL;
 
 /**
  * 用户信息传输过滤器
@@ -19,8 +24,8 @@ public class UserTransmitFilter implements Filter {
     private final StringRedisTemplate stringRedisTemplate;
 
     private static final List<String> IGNORE_URI = Lists.newArrayList(
-            "/api/short-link/v1/user/login",
-            "/api/short-link/v1/user/has-username"
+            "/api/short-link/admin/v1/user/login",
+            "/api/short-link/admin/v1/user/has-username"
     );
 
     @Override
@@ -28,18 +33,28 @@ public class UserTransmitFilter implements Filter {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String requestURI = httpServletRequest.getRequestURI();
         if (!IGNORE_URI.contains(requestURI)) {
-            String username = httpServletRequest.getHeader("username");
-            String token = httpServletRequest.getHeader("token");
+            String method = httpServletRequest.getMethod();
+            if (!(Objects.equals(requestURI, "/api/short-link/admin/v1/user") && Objects.equals(method, "POST"))) {
+                String username = httpServletRequest.getHeader("username");
+                String token = httpServletRequest.getHeader("token");
 
-            Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
-            if (userInfoJsonStr != null) {
+                if (!StrUtil.isAllNotBlank(username, token)) {
+                    throw new ClientException(USER_TOKEN_FAIL);
+                }
+                Object userInfoJsonStr;
+                try {
+                    userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+                    if (userInfoJsonStr == null) throw new ClientException(USER_TOKEN_FAIL);
+                } catch (Exception ex) {
+                    throw new ClientException(USER_TOKEN_FAIL);
+                }
                 UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
                 UserContext.setUser(userInfoDTO);
-            }
-            try {
-                filterChain.doFilter(servletRequest, servletResponse);
-            } finally {
-                UserContext.removeUser();
+                try {
+                    filterChain.doFilter(servletRequest, servletResponse);
+                } finally {
+                    UserContext.removeUser();
+                }
             }
         } else {
             filterChain.doFilter(servletRequest, servletResponse);
--- admin/src/main/java/org/myproject/shortlink/admin/common/enums/UserErrorCodeEnum.java
+++ admin/src/main/java/org/myproject/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -3,6 +3,7 @@ package org.myproject.shortlink.admin.common.enums;
 import org.myproject.shortlink.admin.common.convention.errorcode.IErrorCode;
 
 public enum UserErrorCodeEnum implements IErrorCode {
+    USER_TOKEN_FAIL("A000200", "用户Token用户失败"),
     USER_NOTFOUND("B000200", "用户记录不存在"),
     USER_NAME_EXIST("B000201", "用户名已存在"),
     USER_EXIST("B000202", "用户记录已存在"),
