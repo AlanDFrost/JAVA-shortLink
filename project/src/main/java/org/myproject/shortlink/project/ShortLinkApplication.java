afa07b526d788842e538054b2b18a3255e300b94
zyc <zyc15093213381@163.com>
2026-01-16 16:30:58 +0800

    开发短链接微服务（配置Nacos）

--- project/src/main/java/org/myproject/shortlink/project/ShortLinkApplication.java
+++ project/src/main/java/org/myproject/shortlink/project/ShortLinkApplication.java
@@ -3,8 +3,10 @@ package org.myproject.shortlink.project;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("org.myproject.shortlink.project.dao.mapper")
 public class ShortLinkApplication {
 
