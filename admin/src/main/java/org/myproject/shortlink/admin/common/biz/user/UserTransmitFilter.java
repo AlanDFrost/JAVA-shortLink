5919b160d9588a6d861d25ab2d5f3dfa97d709de
zyc <zyc15093213381@163.com>
2026-01-18 16:11:35 +0800

    开发短链接引入网关，并重构用户鉴权功能至网关

--- admin/src/main/java/org/myproject/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/org/myproject/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,64 +1,35 @@
 package org.myproject.shortlink.admin.common.biz.user;
 
 import cn.hutool.core.util.StrUtil;
-import com.alibaba.fastjson2.JSON;
-import com.google.common.collect.Lists;
-import jakarta.servlet.*;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.myproject.shortlink.admin.common.convention.exception.ClientException;
-import org.springframework.data.redis.core.StringRedisTemplate;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Objects;
-
-import static org.myproject.shortlink.admin.common.enums.UserErrorCodeEnum.USER_TOKEN_FAIL;
+import lombok.SneakyThrows;
 
 /**
  * 用户信息传输过滤器
- *
  */
 @RequiredArgsConstructor
 public class UserTransmitFilter implements Filter {
-    private final StringRedisTemplate stringRedisTemplate;
 
-    private static final List<String> IGNORE_URI = Lists.newArrayList(
-            "/api/short-link/admin/v1/user/login",
-            "/api/short-link/admin/v1/user/has-username",
-            "/api/short-link/admin/v1/user"
-    );
-
+    @SneakyThrows
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        String requestURI = httpServletRequest.getRequestURI();
-        if (!IGNORE_URI.contains(requestURI)) {
-            String method = httpServletRequest.getMethod();
-            if (!(Objects.equals(requestURI, "/api/short-link/admin/v1/user") && Objects.equals(method, "POST"))) {
-                String username = httpServletRequest.getHeader("username");
-                String token = httpServletRequest.getHeader("token");
-
-                if (!StrUtil.isAllNotBlank(username, token)) {
-                    throw new ClientException(USER_TOKEN_FAIL);
-                }
-                Object userInfoJsonStr;
-                try {
-                    userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
-                    if (userInfoJsonStr == null) throw new ClientException(USER_TOKEN_FAIL);
-                } catch (Exception ex) {
-                    throw new ClientException(USER_TOKEN_FAIL);
-                }
-                UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
-                UserContext.setUser(userInfoDTO);
-                try {
-                    filterChain.doFilter(servletRequest, servletResponse);
-                } finally {
-                    UserContext.removeUser();
-                }
-            }
-        } else {
+        String username = httpServletRequest.getHeader("username");
+        if (StrUtil.isNotBlank(username)) {
+            String userId = httpServletRequest.getHeader("userId");
+            String realName = httpServletRequest.getHeader("realName");
+            UserInfoDTO userInfoDTO = UserInfoDTO.builder().userId(userId).userName(username).realName(realName).build();
+            UserContext.setUser(userInfoDTO);
+        }
+        try {
             filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            UserContext.removeUser();
         }
     }
 }
---
+++ gateway/src/main/java/org/myproject/shortlink/gateway/ShortLinkGatewayServiceApplication.java
@@ -0,0 +1,12 @@
+package org.myproject.shortlink.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortLinkGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkGatewayServiceApplication.class, args);
+    }
+}
